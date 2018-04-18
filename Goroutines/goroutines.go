36b412235c1713b48421ecaeffbdd87cb0b9ed73
t00361654 <tanzhuxiaqiu@huawei.com>
2018-04-18 11:36:44 +0800

    add goroutines

---
+++ Goroutines/goroutines.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func f(from string) {
+	for i := 0; i < 5; i++ {
+		fmt.Println(from, ":", i)
+		time.Sleep(time.Millisecond)
+	}
+}
+
+func main() {
+	f("direct")
+
+	go f("goroutine")
+
+	go func(msg string) {
+		fmt.Println(msg)
+	}("going")
+
+	fmt.Scanln()
+	fmt.Println("done")
+}
